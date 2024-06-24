b0eda9848e2f784fda0118cdf91945e277310c7f
Syefoyo <trisyefolionel@gmail.com>
2024-06-24 13:04:21 +0700

    Multiplication Files

---
+++ app.js
@@ -0,0 +1,6 @@
+function result() {
+    const myInput = document.getElementById("myInput").value;
+    const myInputt = document.getElementById("myInput2").value;
+    const text = document.getElementById("test");
+    text.innerHTML = myInput * myInputt;
+}
